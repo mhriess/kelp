e5887ad6270745c7c8d304128c5dc8f4bb166a99
Mike Riess <mikeriess@gmail.com>
2012-12-10 15:41:34 -0800

    Limit faker name to 30 chars

--- spec/factories/restaurants.rb
+++ spec/factories/restaurants.rb
@@ -2,7 +2,7 @@ neighborhoods = ["West Portal", "Nob Hill", "Nopa", "Financial District"]
 
 FactoryGirl.define do
   factory :restaurant do
-    name { Faker::Company.name }
+    name { (Faker::Company.name).slice(0,30) }
     neighborhood { neighborhoods[rand() * 4] }
     address { Faker::Address.street_address }
     city { Faker::Address.city }
