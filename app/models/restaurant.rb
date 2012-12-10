4d388367dd0c649e57c7eafe1436fc2169fb76b3
Mike Riess <mikeriess@gmail.com>
2012-12-10 15:43:21 -0800

    Add validations for presence of Restaurant attributes and specs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,15 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :address, :city, :name, :neighborhood, :state, :zipcode
+  
+  validates :name,
+    :presence => true,
+    :length => { :maximum => 30, :minimum => 2 }
+  validates :neighborhood,
+    :presence => true
+  validates :city,
+    :presence => true
+  validates :state,
+    :presence => true
+  validates :zipcode,
+    :presence => true
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,12 +2,36 @@ require 'spec_helper'
 
 describe Restaurant do
   it "has a valid factory" do
-    create(:restaurant).should be_valid
+    expect(create(:restaurant)).to be_valid
   end
   
-  it "is invalid without a name"
-  it "is invalid without a neighborhood"
-  it "is invalid without a city"
-  it "is invalid without a state"
-  it "is invalid without a zipcode"
+  describe "name" do
+    it "is invalid without a name" do
+      expect(build(:restaurant, name: nil)).to_not be_valid
+    end
+    
+    it "is no longer than 30 characters" do
+      expect(build(:restaurant, name: "#{'a' * 31}")).to_not be_valid
+    end
+    
+    it "is at least 2 characters" do
+      expect(build(:restaurant, name: "A")).to_not be_valid
+    end  
+  end
+  
+  it "is invalid without a neighborhood" do
+    expect(build(:restaurant, neighborhood: nil)).to_not be_valid
+  end
+  
+  it "is invalid without a city" do
+    expect(build(:restaurant, city: nil)).to_not be_valid 
+  end
+  
+  it "is invalid without a state" do
+    expect(build(:restaurant, state: nil)).to_not be_valid
+  end
+  
+  it "is invalid without a zipcode" do
+    expect(build(:restaurant, zipcode: nil)).to_not be_valid
+  end
 end
