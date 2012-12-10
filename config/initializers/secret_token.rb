d92ed3f5ad4a7f4ac9dd016cbfac07ee88531f0c
Mike Riess <mikeriess@gmail.com>
2012-12-10 14:57:44 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kelp::Application.config.secret_token = '60e7e313f0ea05f29f90377244ffa2715dfef324d9cc7c258f74f1c051be82701087d73c84fcd4c252cff670759069e8164cd62e64235ecc417c97f693a79e62'
