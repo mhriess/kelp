bcb0a93a0f56ba1baa6c2d934afbc3b6272021b6
Mike Riess <mikeriess@gmail.com>
2012-12-11 14:36:58 -0800

    Add ratings to Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121211222308) do
+ActiveRecord::Schema.define(:version => 20121211223438) do
 
   create_table "restaurants", :force => true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20121211222308) do
     t.integer  "restaurant_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.integer  "ratings"
   end
 
 end
