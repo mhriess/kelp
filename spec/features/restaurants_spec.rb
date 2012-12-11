29001e9eac7fea64fc2240611afdd12e9765fbee
Mike Riess <mikeriess@gmail.com>
2012-12-10 16:20:05 -0800

    Add restaurant integration specs, show and index controller methods, basic index and show views.

--- spec/features/restaurants_spec.rb
+++ spec/features/restaurants_spec.rb
@@ -1,21 +1,18 @@
 require 'spec_helper'
 
 describe "Restaurants" do
+  let! (:sushi_one) { create(:restaurant) }
+  let! (:sushi_two) { create(:restaurant) }
+  
   context "visiting the root path" do
     it "displays all restaurants" do
-      sushi_one = create(:restaurant)  
-      sushi_two = create(:restaurant)
-      
       visit root_path
       
       page.should have_content(sushi_one.name)
       page.should have_content(sushi_two.name)
     end
     
-    it "displays each restaurant's information" do
-      sushi_one = create(:restaurant)  
-      sushi_two = create(:restaurant)
-      
+    it "displays each restaurant's information" do      
       visit root_path
       
       page.should have_content(sushi_one.name)
@@ -31,6 +28,18 @@ describe "Restaurants" do
       page.should have_content(sushi_two.city)
       page.should have_content(sushi_two.state)
       page.should have_content(sushi_two.zipcode)
+    end
+  end
+  
+  context "viewing a restaurant" do
+    it "allows a user to view a specific restaurant's info" do
+      visit root_path
+      
+      click_link sushi_one.name
+            
+      current_path.should eq restaurant_path(sushi_one)
+      
+      page.should have_content sushi_one.name
     end
   end
 end
